b30b738274eea039743886caa6b4a89614d31982
Penny Ho <hopenny00@gmail.com>
2022-11-11 13:27:11 +0800

    test3

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-    $add = "test";
-	$add2 = "test2"
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+$add = "test";
+$add2 = "test2";
+$add3 = "test3";
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+} else {
+    $uri = 'http://';
+}
+$uri .= $_SERVER['HTTP_HOST'];
+header('Location: ' . $uri . '/dashboard/');
+exit;
 ?>
 Something is wrong with the XAMPP installation :-(
